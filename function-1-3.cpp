3129123d44892709c0bac342877d5312bbdc893c
BaileyS123 <baileystaykov@gmail.com>
2024-03-13 16:15:25 +1030

    Add files via upload

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -1,5 +1,7 @@
 double* duplicateArray(double* array, int size){
-    double* array2 = new double [size];
-    array2 = array;
+    double* array2 = new double[size];
+    for (int i = 0; i < size; i++){
+        array2[i] = array[i];
+    }
     return array2;
 }
